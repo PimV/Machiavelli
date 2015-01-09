5c17b3cd4d177a409dda17dfef7a0f21caab0470
Pim Verlangen <pim.verlangen@gmail.com>
2015-01-09 11:43:33 +0100

    removed hacky non-mem-leak fix

--- Machiavelli/Machiavelli/Main.cpp
+++ Machiavelli/Machiavelli/Main.cpp
@@ -10,11 +10,11 @@ void WINAPI ConsoleHandler(DWORD CEvent)
 {
 	if (CEvent == CTRL_CLOSE_EVENT)
 	{
-		std::cout << "No memory leaks detected.\r\n" << std::endl;
-		std::cout << "Visual Leak Detector is now exiting.\r\n" << std::endl;
-		
-		OutputDebugStringW(L"No memory leaks detected.\r\n");
-		OutputDebugStringW(L"Visual Leak Detector is now exiting.\r\n");
+		//std::cout << "No memory leaks detected.\r\n" << std::endl;
+		//std::cout << "Visual Leak Detector is now exiting.\r\n" << std::endl;
+		//
+		//OutputDebugStringW(L"No memory leaks detected.\r\n");
+		//OutputDebugStringW(L"Visual Leak Detector is now exiting.\r\n");
 	}
 	
 }
@@ -22,13 +22,15 @@ void WINAPI ConsoleHandler(DWORD CEvent)
 int main(int argc, const char * argv[])
 {
 	//Fix all memory leaks
-	std::cout << "Visual Leak Detector Version 2.4RC2-PIMV installed.\r\n" << std::endl;
-	OutputDebugStringW(L"Visual Leak Detector Version 2.4RC2-PIMV installed.\r\n");
+	//std::cout << "Visual Leak Detector Version 2.4RC2-PIMV installed.\r\n" << std::endl;
+	//OutputDebugStringW(L"Visual Leak Detector Version 2.4RC2-PIMV installed.\r\n");
 
-	if (SetConsoleCtrlHandler((PHANDLER_ROUTINE)ConsoleHandler, TRUE) == FALSE)
-	{
-		
-	}
+	//if (SetConsoleCtrlHandler((PHANDLER_ROUTINE)ConsoleHandler, TRUE) == FALSE)
+	//{
+	//	
+	//}
+
+
 	std::shared_ptr<Game> game = std::make_shared<Game>();
 	std::shared_ptr<InputHandler> inputHandler = std::make_shared<InputHandler>();
 	
@@ -36,18 +38,14 @@ int main(int argc, const char * argv[])
 	
 	Server::Instance().setInputHandler(inputHandler);
 	Server::Instance().setGame(game);
-	Server::Instance().run();
-	
-	//std::string input;
-	//while (input != "quit") {
-	//	std::cin >> input;
-	//	
-	//}
-	//Server::Instance().stop();
-	//
-	//std::getchar();
+	//Server::Instance().run();
 
-	
+	std::string input;
+	while (input != "quit") {
+		std::cin >> input;
+	}
+
+
 
 	return 0;
 }
